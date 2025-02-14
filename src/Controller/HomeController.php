036fbd9c22c36351f31f5cf306365f83732107b6
marceau07 <marceau0707@gmail.com>
2025-02-14 12:23:02 +0100

    Configuration de la route par défaut

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\VideoRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/', name: 'app_home')]
+    public function index(VideoRepository $videoRepository): Response
+    {
+        return $this->render('home/index.html.twig', [
+            'latest_videos' => $videoRepository->findLatest(),
+        ]);
+    }
+}
